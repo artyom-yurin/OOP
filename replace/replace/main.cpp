7b3d25eb18ab75defcb816b23b84a9ab1f590205
artyom-yurin <artem_yrin@mail.ru>
2016-09-17 01:46:11 +0400

    LW1 program Replace
    Signed-off-by: artyom-yurin <artem_yrin@mail.ru>

---
+++ replace/replace/main.cpp
@@ -0,0 +1,67 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char * argv[])
+{
+	if (argc != 5)
+	{
+		cout << "Invalid arguments count\n"
+			<< "Usage: replace.exe <input file> <output file> <search string> <replace string>\n";
+		return 1;
+	}
+
+	ifstream input(argv[1]);
+
+	if (!input.is_open())
+	{
+		cout << "Failed to open " << argv[1] << " for reading\n";
+		return 1;
+	}
+
+	string searchString = argv[3];
+
+	if (searchString == "")
+	{
+		cout << "Search string can not be empty\n";
+		return 1;
+	}
+
+	string replaceString = argv[4];
+
+	ofstream output(argv[2]);
+
+	if (!output.is_open())
+	{
+		cout << "Failed to open " << argv[2] << " for writing\n";
+		return 1;
+	}
+
+	string currentString = "";
+
+	while (getline(input, currentString))
+	{
+		int positionString = currentString.find(searchString, 0);
+
+		while (positionString != string::npos)
+		{
+			currentString.replace(positionString, searchString.size(), replaceString);
+			positionString = currentString.find(searchString, positionString);
+		}
+		if (!(output << currentString << "\n"))
+		{
+			cout << "Failed to save data on disk\n";
+			return 1;
+		}
+	}
+
+	if (!output.flush())
+	{
+		cout << "Failed to save data on disk\n";
+		return 1;
+	}
+
+	return 0;
+}
