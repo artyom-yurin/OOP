d67ae8b55e94025d070e69befb956f1beca4f8c0
Artyom <artem_yrin@mail.ru>
2016-09-24 12:22:42 +0400

    Add new tests
    Signed-off-by: Artyom <artem_yrin@mail.ru>

--- flipbyte/flipbyte/test.bat
+++ flipbyte/flipbyte/test.bat
@@ -7,12 +7,26 @@ fc.exe "%TEMP%\output.txt" correctAnswers\correctInvalidArguments.txt
 if ERRORLEVEL 1 goto err
 echo Test for invalid argements succeed!
                                                                                                                           
-rem invalid value 
+rem invalid value 256 
 %PROGRAM% "256" > "%TEMP%\output.txt"
 if NOT ERRORLEVEL 1 goto err
 fc.exe "%TEMP%\output.txt" correctAnswers\correctInvalidValue.txt
 if ERRORLEVEL 1 goto err
-echo Test for invalid value succeed!
+echo Test for invalid value 256 succeed!
+
+rem invalid value -1
+%PROGRAM% "-1" > "%TEMP%\output.txt"
+if NOT ERRORLEVEL 1 goto err
+fc.exe "%TEMP%\output.txt" correctAnswers\correctInvalidValue.txt
+if ERRORLEVEL 1 goto err
+echo Test for invalid value -1 succeed!
+
+rem string value
+%PROGRAM% "hello" > "%TEMP%\output.txt"
+if NOT ERRORLEVEL 1 goto err
+fc.exe "%TEMP%\output.txt" correctAnswers\correctInvalidValue.txt
+if ERRORLEVEL 1 goto err
+echo Test for string value succeed!
 
 rem flip byte 6
 %PROGRAM% "6" > "%TEMP%\output.txt"
@@ -21,14 +35,14 @@ fc.exe "%TEMP%\output.txt" correctAnswers\correctValue6.txt
 if ERRORLEVEL 1 goto err
 echo Test for flip byte 6 succeed!
 
-rem flip byte 6
+rem flip byte 0
 %PROGRAM% "0" > "%TEMP%\output.txt"
 if ERRORLEVEL 1 goto err
 fc.exe "%TEMP%\output.txt" correctAnswers\correctValue0.txt 
 if ERRORLEVEL 1 goto err
 echo Test for flip byte 0 succeed!
 
-rem flip byte 6
+rem flip byte 255
 %PROGRAM% "255" > "%TEMP%\output.txt"
 if ERRORLEVEL 1 goto err
 fc.exe "%TEMP%\output.txt" correctAnswers\correctValue255.txt 
