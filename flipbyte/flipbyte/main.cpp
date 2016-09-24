364abe0ec0da5e40d38f544d1d3534b0a897efc0
Artyom <artem_yrin@mail.ru>
2016-09-24 13:37:54 +0400

    Replaced printf to cout
    Signed-off-by: Artyom <artem_yrin@mail.ru>

--- flipbyte/flipbyte/main.cpp
+++ flipbyte/flipbyte/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 namespace
 {
-	bool isNumber(char * str)
+	bool IsNumber(char * str)
 	{
 		for (int i = 0; i < strlen(str); ++i)
 		{
@@ -16,7 +16,7 @@ namespace
 		return true;
 	}
 	
-	void flipByte(uint8_t & byte)
+	void FlipByte(uint8_t & byte)
 	{
 		byte = (byte & 0b01010101 << 1) | (byte & 0b10101010 >> 1);
 		byte = (byte & 0b00110011 << 2) | (byte & 0b11001100 >> 2);
@@ -32,7 +32,7 @@ int main(int argc, char * argv[])
 			<< "Usage: flipbyte.exe <input byte>\n";
 		return 1;
 	};
-	if ((!isNumber(argv[1])) || !((atoi(argv[1]) <= 255) && (atoi(argv[1]) >= 0)))
+	if ((!IsNumber(argv[1])) || !((atoi(argv[1]) <= 255) && (atoi(argv[1]) >= 0)))
 	{
 		cout << "Invalid value\n"
 			<< "<input byte> must be unsigned int(0 .. 255)\n";
@@ -41,8 +41,8 @@ int main(int argc, char * argv[])
 
 	uint8_t byte = static_cast <uint8_t> (atoi(argv[1]));
 
-	printf("Input byte: %u\n", byte);
-	flipByte(byte);
-	printf("Output byte: %u\n", byte);
+	cout << "Input byte: " << argv[1] << endl;
+	FlipByte(byte);
+	cout << "Output byte: " << static_cast <int> (byte) << endl;
 	return 0;
 }
