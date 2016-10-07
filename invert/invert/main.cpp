7b9dfe3894424601de85a49c842e095cc0d8f0e9
Artyom <artem_yrin@mail.ru>
2016-10-08 00:23:29 +0400

    Added function GetDeterminant, but we can lose precision.
    Signed-off-by: Artyom <artem_yrin@mail.ru>

--- invert/invert/main.cpp
+++ invert/invert/main.cpp
@@ -25,6 +25,44 @@ float GetDeterminant3(const Matrix & matrix)
 		matrix[0][1] * matrix[1][0] * matrix[2][2];
 }
 
+//float GetDeterminant(const Matrix & matrix)
+//{
+//	float determinant = 0.f;
+//	size_t size = matrix.size();
+//	Matrix currentMatrix(size, vector <float>(size, 0.f));
+//	currentMatrix = matrix;
+//
+//	float degree = 0;
+//	for (size_t i = 0; i < size - 1; ++i)
+//	{
+//		size_t t = 1;
+//		while (currentMatrix[i][i] == 0)
+//		{
+//			for (size_t j = 0; j < size; ++j)
+//			{
+//				currentMatrix[i][j] = determinant;
+//				currentMatrix[i][j] = currentMatrix[i + t][j];
+//				currentMatrix[i + t][j] = determinant;
+//			}
+//			degree++;
+//			t++;
+//		}
+//
+//		for (size_t k = i + 1; k < size; ++k)
+//		{
+//			determinant = currentMatrix[k][i] / currentMatrix[i][i];
+//			for (size_t j = 0; j < size; j++)
+//				currentMatrix[k][j] -= currentMatrix[i][j] * determinant;
+//		}
+//	}
+//
+//	determinant = pow(-1.f, degree);
+//	for (size_t i = 0; i < size; ++i)
+//		determinant *= currentMatrix[i][i];
+//
+//	return determinant;
+//}
+
 Matrix GetMinor(const Matrix &matrix, size_t row, size_t column)
 {
 	Matrix minor (MATRIX_DIMENSION2, vector <float> (MATRIX_DIMENSION2, 0.f));
