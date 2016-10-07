52a7b5009406230a8d1521dbc5a5552d0dfcb693
Artyom <artem_yrin@mail.ru>
2016-10-07 23:24:06 +0400

    Rewrote function GetAlgebraicAdditionsMatrix
    Signed-off-by: Artyom <artem_yrin@mail.ru>

--- invert/invert/main.cpp
+++ invert/invert/main.cpp
@@ -97,13 +97,21 @@ bool CanReadMatrixFromFile(ifstream & file, Matrix & matrix)
 
 Matrix GetAlgebraicAdditionsMatrix(const Matrix &matrix)
 {
-	return{ 
+	Matrix newMatrix(MATRIX_DIMENSION3, vector <float>(MATRIX_DIMENSION3, 0.f));
+
+	for (size_t i = 0; i < matrix.size(); ++i)
+	{
+		for (size_t j = 0; j < matrix.size(); ++j)
 		{
-			{matrix[0][0], -matrix[0][1], matrix[0][2]},
-			{-matrix[1][0], matrix[1][1], -matrix[1][2]},
-			{matrix[2][0], -matrix[2][1], matrix[2][2]}
+			newMatrix[i][j] = matrix[i][j];
+			if (((matrix.size() * i) + (j + 1)) % 2 == 0)
+			{
+				newMatrix[i][j] *= -1;
+			}
 		}
-	};
+	}
+
+	return newMatrix;
 }
 
 Matrix GetAlgebraicAdditionsMatrixTransposed(const Matrix &matrix)
