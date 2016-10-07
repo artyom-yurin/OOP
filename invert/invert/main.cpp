e50593e135bbb494756f176fd778a21645c0ee9a
Artyom <artem_yrin@mail.ru>
2016-10-07 23:01:33 +0400

    Rewrote function GetAlgebraicAdditionsMatrixTransposed
    Signed-off-by: Artyom <artem_yrin@mail.ru>

--- invert/invert/main.cpp
+++ invert/invert/main.cpp
@@ -108,13 +108,17 @@ Matrix GetAlgebraicAdditionsMatrix(const Matrix &matrix)
 
 Matrix GetAlgebraicAdditionsMatrixTransposed(const Matrix &matrix)
 {
-	return{
+	Matrix newMatrix(MATRIX_DIMENSION3, vector <float>(MATRIX_DIMENSION3, 0.f));
+
+	for (size_t i = 0; i < matrix.size(); ++i)
+	{
+		for (size_t j = 0; j < matrix.size(); ++j)
 		{
-			{matrix[0][0], matrix[1][0], matrix[2][0]},
-			{matrix [0][1], matrix[1][1], matrix[2][1]},
-			{matrix[0][2], matrix[1][2], matrix[2][2]}
+			newMatrix[i][j] = matrix[j][i];
 		}
-	};
+	}
+
+	return newMatrix;
 }
 
 Matrix MultiplicationScalarOnMatrix(const Matrix & matrix, float scalar)
