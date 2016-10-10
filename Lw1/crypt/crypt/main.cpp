aa260337fd19d019936d62d05f372885f81fea0c
Artyom <artem_yrin@mail.ru>
2016-10-10 22:01:49 +0400

    Added task crypt
    Signed-off-by: Artyom <artem_yrin@mail.ru>

---
+++ Lw1/crypt/crypt/main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+const int CRYPT = 0;
+const int DECRYPT = 1;
+
+
+bool IsNumber(char * str)
+{
+	for (int i = 0; i < strlen(str); ++i)
+	{
+		if (!isdigit(str[i])) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int main(int argc, char * argv [])
+{
+	if (argc != 5)
+	{
+		cout << "Invalid arguments count\n"
+			<< "Usage: crypt.exe crypt <input file> <output file> <key>\n"
+			<< "or\n"
+			<< "crypt.exe decrypt <input file> <output file> <key>\n";
+		return 1;
+	}
+	int mode;
+	if (argv[1] = "crypt")
+	{
+		mode = CRYPT;
+	} 
+	else if (argv[1] = "decrypt")
+	{
+		mode = DECRYPT;
+	}
+	else
+	{
+		cout << "Unknown mode\n";
+		return 1;
+	}
+
+	ifstream input(argv[2]);
+
+	if (!input.is_open())
+	{
+		cout << "Failed to open " << argv[2] << " for writing\n";
+		return 1;
+	}
+
+	ofstream output(argv[3]);
+
+	if (!output.is_open())
+	{
+		cout << "Failed to open " << argv[3] << " for writing\n";
+		return 1;
+	}
+
+	if (!IsNumber(argv[4]) && ((atoi(argv[4]) >= 0) && (atoi(argv[4]) <= 255)))
+	{
+		cout << "Key must be 0 to 255\n";
+		return 1;
+	}
+	int key = atoi(argv[4]);
+
+
+
+	return 0;
+}
