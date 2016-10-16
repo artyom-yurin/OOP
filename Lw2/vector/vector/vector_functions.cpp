6267b4451959ae4d8ec5651c963b94a496a16720
Artyom <artem_yrin@mail.ru>
2016-10-16 19:13:47 +0300

    Added file cpp with finctions' declaration
    Signed-off-by: Artyom <artem_yrin@mail.ru>

---
+++ Lw2/vector/vector/vector_functions.cpp
@@ -0,0 +1,49 @@
+#include "vector_functions.h"
+
+std::vector <double> GetNumbers(std::istream & input)
+{
+	std::vector <double> result;
+	copy(std::istream_iterator <double>(input), std::istream_iterator <double>(), back_inserter(result));
+	return result;
+}
+
+auto GetMaxMinElement(const std::vector <double> & numbers)
+{
+	return std::minmax_element(numbers.begin(), numbers.end());
+}
+
+void ProcessVector(std::vector <double> & numbers)
+{
+	if (numbers.empty())
+	{
+		return;
+	}
+
+	double multiplier = GetMultiplier(numbers);
+
+	transform(numbers.begin(), numbers.end(), numbers.begin(), [=](double element) {
+		return element * multiplier;
+	});
+}
+
+void PrintVector(std::ostream & output, const std::vector <double> & numbers)
+{
+	for (const double & element : numbers)
+	{
+		output << std::fixed << std::setprecision(3) << element << " ";
+	}
+	output << std::endl;
+}
+
+double GetMultiplier(const std::vector <double> & numbers)
+{
+	auto minMax = GetMaxMinElement(numbers);
+	double result = *minMax.second;
+
+	if (*minMax.first != 0)
+	{
+		result /= *minMax.first;
+	}
+
+	return result;
+}
--- Lw2/vector/vector/vector_functions.h
+++ Lw2/vector/vector/vector_functions.h
@@ -5,52 +5,13 @@
 #include <string>
 #include <iomanip>
 #include <iterator>
-#include <iostream>
 
-std::vector <double> GetNumbers(std::istream & input)
-{
-	std::vector <double> result;
-	copy(std::istream_iterator <double>(input), std::istream_iterator <double>(), back_inserter(result));
-	return result;
-}
+std::vector <double> GetNumbers(std::istream & input);
 
-auto GetMaxMinElement(const std::vector <double> & numbers)
-{
-	return std::minmax_element(numbers.begin(), numbers.end());
-}
+auto GetMaxMinElement(const std::vector <double> & numbers);
 
-double GetMultiplier(const std::vector <double> & numbers)
-{
-	auto minMax = GetMaxMinElement(numbers);
-	double result = *minMax.second;
+double GetMultiplier(const std::vector <double> & numbers);
 
-	if (*minMax.first != 0)
-	{
-		result /= *minMax.first;
-	}
+void ProcessVector(std::vector <double> & numbers);
 
-	return result;
-}
-
-void ProcessVector(std::vector <double> & numbers)
-{
-	if (numbers.empty())
-	{
-		return;
-	}
-
-	double multiplier = GetMultiplier(numbers);
-
-	transform(numbers.begin(), numbers.end(), numbers.begin(), [=](double element) {
-		return element * multiplier;
-	});
-}
-
-void PrintVector(std::ostream & output, const std::vector <double> & numbers)
-{
-	for (const double & element : numbers)
-	{
-		output << std::fixed << std::setprecision(3) << element << " ";
-	}
-	output << std::endl;
-}
+void PrintVector(std::ostream & output, const std::vector <double> & numbers);
