86084383ab673557eec3cff7ca9a9a023aa6854c
Artyom <artem_yrin@mail.ru>
2016-10-20 09:00:57 +0300

    Added files
    Signed-off-by: Artyom <artem_yrin@mail.ru>

---
+++ Lw2/MiniDictionary/dictionary/dictionary_functions.cpp
@@ -0,0 +1,81 @@
+#include "stdafx.h"
+#include "dictionary_functions.h"
+
+void GetDictionaryFromFile(std::ifstream & input, std::map <std::string, std::string> & dictionary)
+{
+	std::string key = "";
+	std::string value = "";
+	while (getline(input, key, ':') && getline(input, value, '\n'))
+	{
+		dictionary[key] = value;
+	}
+}
+
+bool CheckExit(const std::string & word)
+{
+	return word == "...";
+}
+
+bool AddNewWord(std::istream & input, std::ostream & output, std::map<std::string, std::string>& dictionary, const std::string & key)
+{
+	std::string value = GetWord(input);
+	if (value.empty())
+	{ 
+		output << "Слово \"" << key << "\" было проигнорировано.\n";
+		return false;
+	}
+	std::transform(value.begin(), value.end(), value.begin(), ::tolower);
+	dictionary[key] = value; 
+	return true;
+}
+
+bool GetTranslate(std::ostream & output, const std::map <std::string, std::string> & dictionary, const std::string & word)
+{	
+	bool result = false;
+	if (dictionary.find(word) != dictionary.end())
+	{
+		output << dictionary.find(word)->second;
+		result = true;
+	}
+	else
+	{
+		for (auto & element : dictionary)
+		{
+			if (element.second == word)
+			{
+				if (result == true)
+				{
+					output << ", ";
+				}
+				output << element.first;
+				result = true;
+			}
+		}
+	}
+	if (result == true)
+	{
+		output << "\n";
+	}
+	return result;
+}
+
+bool SaveAnswer()
+{
+	std::cout << "В словарь были внесены изменения. Введите Y или y для сохранения перед выходом.\n";
+	std::string answer = "";
+	std::cin >> answer;
+	if (answer == "Y" || answer == "y")
+	{
+		return true;
+	}
+	return false;
+}
+
+void SaveDictionaryInFile(const std::map<std::string, std::string>& dictionary, std::ofstream & output)
+{
+	for (auto it = dictionary.begin(); it != dictionary.end(); it++)
+	{
+		output << it->first << ":" << it->second << "\n";
+	}
+}
+
