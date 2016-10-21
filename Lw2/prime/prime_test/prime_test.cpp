5bd19ead285303c2d785ef4cf1711730844b24a9
Artyom <artem_yrin@mail.ru>
2016-10-21 21:11:46 +0300

    Added test
    Signed-off-by: Artyom <artem_yrin@mail.ru>

---
+++ Lw2/prime/prime_test/prime_test.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+
+#include "../prime/prime_functions.cpp"
+
+BOOST_AUTO_TEST_SUITE(GeneratePrimeNumbersSet_function)
+
+	BOOST_AUTO_TEST_CASE(correct_data)
+	{
+		std::set<int> primeNumbers = GeneratePrimeNumbersSet(10);
+		std::set<int> correctPrimeNumbers = {
+			2, 3, 5, 7
+		};
+		BOOST_CHECK(primeNumbers == correctPrimeNumbers);
+	}
+
+BOOST_AUTO_TEST_SUITE_END()
