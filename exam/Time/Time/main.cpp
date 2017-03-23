24bfb74310869e23b5846d7316256543ba924cfa
Artyom <artem_yrin@mail.ru>
2017-03-23 12:41:26 +0300

    Добавил возможность хранить больше чем 1 день
    Signed-off-by: Artyom <artem_yrin@mail.ru>

--- exam/Time/Time/main.cpp
+++ exam/Time/Time/main.cpp
@@ -2,14 +2,18 @@
 #include <iostream>
 using namespace std;
 
+const int MAX_HOUR = 23;
+const int MINUTE_IN_HOUR = 59;
+const int SECONDS_IN_MINUTE = 59;
+
 std::string FormatNumber(int number)
 {
 	return (number <= 9 ? "0" + std::to_string(number) : std::to_string(number));
 }
 
-bool InRange(int number, int UpperLimit, int LowerLimit)
+bool InRange(int number, int LowerLimit, int UpperLimit)
 {
-	return (number <= UpperLimit && number >= LowerLimit);
+	return (number >= LowerLimit && number <= UpperLimit);
 }
 
 class CTime
@@ -20,24 +24,20 @@ public:
 	// исключение invalid_argument
 	explicit CTime(int timeStamp)
 	{
-		if (!InRange(timeStamp, 86399, 0))
-		{
-			throw std::invalid_argument("Timestamp must be from 0 to 86399");
-		}
 		m_timeStamp = timeStamp;
 	}
 
 	CTime(int hours, int minutes, int seconds)
 	{
-		if (!InRange(hours, 23, 0))
+		if (!InRange(hours, 0, MAX_HOUR))
 		{
 			throw std::invalid_argument("Hours must be from 0 to 23");
 		}
-		if (!InRange(minutes, 59, 0))
+		if (!InRange(minutes, 0, MINUTE_IN_HOUR))
 		{
 			throw std::invalid_argument("Minutes must be from 0 to 59");
 		}
-		if (!InRange(seconds, 59, 0))
+		if (!InRange(seconds, 0, SECONDS_IN_MINUTE))
 		{
 			throw std::invalid_argument("Seconds must be from 0 to 59");
 		}
@@ -47,14 +47,14 @@ public:
 	// Возвращает количество часов (0..23)
 	int GetHours()const
 	{
-		return m_timeStamp / 3600;
+		return (m_timeStamp / 3600) % 24;
 		// Написать  недостающий код
 	}
 
 	// Возвращает количество минут (0..59)
 	int GetMinutes()const
 	{
-		return (m_timeStamp % 3600) / 60;
+		return (m_timeStamp / 60) % 60;
 		// Написать  недостающий код
 	}
 
@@ -71,29 +71,36 @@ public:
 		std::string hours = FormatNumber(GetHours());
 		std::string minutes = FormatNumber(GetMinutes());
 		std::string seconds = FormatNumber(GetSeconds());
-		return{
-			hours + ":" + minutes + ":" + seconds
-		};
+		return
+			hours + ":" + minutes + ":" + seconds;
 	}
 
 	int GetTimeStamp()const
 	{
+		++callCount;
 		return m_timeStamp;
 	}
 
-	const CTime operator+=(const CTime & summand)
+	CTime & operator+=(const CTime & summand)
 	{
 		m_timeStamp = (m_timeStamp + summand.m_timeStamp) % (24 * 60 * 60);
 		return *this;
 	}
 
-	const CTime operator-=(const CTime & subtrahend)
+	CTime & operator-=(const CTime & subtrahend)
 	{
 		m_timeStamp = (m_timeStamp - subtrahend.m_timeStamp + (24 * 60 * 60)) % (24 * 60 * 60);
 		return *this;
 	}
 
+	int GetCallCount()const
+	{
+		int result = callCount;
+		return result;
+	}
+
 private:
+	mutable int callCount = 0;
 	int m_timeStamp = 0;
 };
 
@@ -113,6 +120,7 @@ std::ostream & operator<<(std::ostream & output, const CTime & time)
 	return output;
 }
 
+
 /*
 Реализовать методы класса и перечисленные ниже операции. Сложение и вычитание выполняется в пределах суток
 Примеры:
@@ -125,6 +133,8 @@ std::ostream & operator<<(std::ostream & output, const CTime & time)
 
 int main()
 {
-
+	CTime t(0, 10, 0);
+	(t += CTime(1, 0, 0)) += CTime(23, 1, 1);
+	std::cout << t << std::endl;
 	return 0;
 }
